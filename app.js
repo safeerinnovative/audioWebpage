dee1d58332cf3e1364d77b99d8d3ea0f31550448
safeer-innovative <safeer@innovative-net.com>
2022-02-11 02:06:41 +0500

    restricted for button

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 let music = document.getElementById("music");
 let musicOn = document.getElementById('playing');
 let musicOff = document.getElementById('stopped');
+let counter = 0;
 
 function runBody(){
-    music.play();
+    if(counter === 0){
+        music.play();
+        music.muted = false;
+        musicOn.classList.add('show')
+        musicOff.classList.remove('show')
+    }
+    ++counter;
+}
+function startMusic(){
     music.muted = false;
     musicOn.classList.add('show')
     musicOff.classList.remove('show')
